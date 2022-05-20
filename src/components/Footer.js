943a2f0fc74d83e3c65361439dffd383961e3f9d
Kevin R.V <kevinrodriguezvalentin@gmail.com>
2022-05-20 17:43:52 +0100

    Botón RSS añadido, a la espera de RSS propio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import ytlogo from "../SVG/youtube.svg";
 import wlogo from "../SVG/world.svg";
 import twtlogo from "../SVG/twitter-filled.svg";
 import ftlogo from "../SVG/facebook-original.svg";
+import rsslogo from "../SVG/rss-icon.svg";
 
 export default function Footer() {
   return (
@@ -16,13 +17,16 @@ export default function Footer() {
               <a href="https://www.youtube.com/channel/UCsXpM6GoxhFjlGDTyEZ4q8A"><img className="icon" src={ytlogo}/><span class="label"> Youtube</span></a>
             </dd>
             <dd>
-              <a href="https://www.facebook.com/fundacionlidiagarcia/posts/sabios-gu%C3%ADas-interpretes-en-agaete-hoy-el-equipo-t%C3%A9cnico-del-proyecto-sabios-gu%C3%AD/877161022843884/"><img className="icon" src={ftlogo}/><span class="label"> Facebook</span></a>
+              <a href="https://www.facebook.com/fundacionlidiagarcia/posts/sabios-gu%C3%ADas-interpretes-en-agaete-hoy-el-equipo-t%C3%A9cnico-del-proyecto-sabios-gu%C3%AD/877161022843884/"><img className="icon" src={ftlogo}/><span class="label">Facebook</span></a>
             </dd>
             <dd>
-              <a href="https://twitter.com/FundacionLidia"><img className="icon" src={twtlogo}/><span class="label">  Twitter</span></a>
+              <a href="https://twitter.com/FundacionLidia"><img className="icon" src={twtlogo}/><span class="label">Twitter</span></a>
             </dd>
             <dd>
-              <a href="https://www.sabiosguiasinterpretes.com/"><img className="icon" src={wlogo}/><span class="label"> Web Oficial</span></a>
+              <a href="https://www.sabiosguiasinterpretes.com/"><img className="icon" src={wlogo}/><span class="label">Web Oficial</span></a>
+            </dd>
+            <dd>
+              <a href="https://www.sabiosguiasinterpretes.com/feed"><img className="icon" src={rsslogo}/><span class="label">RSS</span> </a>
             </dd>
           </dl>
         </div>
